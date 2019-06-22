1e0b84157ee14bf6da37edd775ea4908f5ee70d3
randler <randlersi@gmail.com>
2019-06-22 12:44:35 -0300

    consumindo dados da API - Codificar no redux

--- src/components/list/ListBugets.js
+++ src/components/list/ListBugets.js
@@ -1,27 +1,44 @@
-import React from 'react'
+import React, { useEffect }  from 'react'
 import { Tab, Row, Col } from 'react-bootstrap';
 
-import { useSelector } from 'react-redux';
+import axios from 'axios';
+import { HOME_API } from '../../utils/variables/variables';
+import { useDispatch, useSelector } from 'react-redux';
+
 import BudgetItem from './BudgetItem';
 import BudgetBody from './BudgetBody';
 
 function ListBugets() {
 
     const budgets = useSelector(state => state.data);
+    const dispatch = useDispatch();
 
+    useEffect(() => {
+      async function fetchData() {
+        // You can await here
+        await axios.get(HOME_API)
+        .then(result =>{
+          dispatch({type: 'ADD_BUDGETS', data: result.data});
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      }
+      fetchData();
+    }, []);
     return (
         <div className="m-3" >
             <Tab.Container  id="list-group-tabs-example">
                 <Row>
                     <Col xs={3}>
                         {budgets.map(budget =>  (
-                        <BudgetItem budget={budget} />
+                        <BudgetItem key={budget} budget={budget} />
                         )
                     )}
                     </Col>
                     <Col xs={9}>
                         {budgets.map(budget =>  (
-                            <BudgetBody budget={budget} />
+                            <BudgetBody key={budget} budget={budget} />
                             )
                         )}
                     </Col>
