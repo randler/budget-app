4d8b63212a7eb5a782727c95cc3a1eda75358cbf
randler <randlersi@gmail.com>
2019-06-23 11:27:42 -0300

    grafico adicionado

--- src/pages/Diagnostic.js
+++ src/pages/Diagnostic.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Menu from '../components/menu/Menu';
+import ChartCanvas from '../components/chart/ChartCanvas';
 
 function Diagnostic() {
   return (
     <div className="Diagnostic">
         <Menu />
-      Hello Diagnóstico!
+        <ChartCanvas />
     </div>
   );
 }
